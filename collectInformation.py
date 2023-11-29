b616aea8f99b1398675165e18e8c8a1be076de16
王翊語 <elva108062223@gapp.nthu.edu.tw>
2023-11-29 00:21:43 +0000

    Modify collectInformation.py

--- collectInformation.py
+++ collectInformation.py
@@ -2,10 +2,14 @@ from pydriller import Repository
 import csv
 
 with open('./pydrillerData/my.csv', 'w', newline='') as file:
-    fields = ['msg', 'files', 'lines', 'committer_date', 'modified_files', 'merge']
+    fields = ['msg', 'files', 'lines', 'committer_date', 'modified_files', 'merge', 'branches']
     writer = csv.DictWriter(file, fieldnames = fields)
-    for commit in Repository('https://github.com/yuawn/NTU-Computer-Security', only_in_branch='master').traverse_commits():
+    writer.writerow({'msg': 'msg', 'files': 'files', 'lines': 'lines', 'committer_date': 'committer_date', 'modified_files': 'modified_file', 'merge': 'merge', 'branches': 'branches'})
+    for commit in Repository('https://github.com/apache/iceberg').traverse_commits():
         modified_file = []
+        branches = []
         for file in commit.modified_files:
             modified_file.append(file.filename)
-        writer.writerow({'msg': commit.msg, 'files': commit.files, 'lines': commit.lines, 'committer_date': commit.committer_date, 'modified_files': modified_file, 'merge': commit.merge})
+        for branch in commit.branches:
+            branches.append(branch)
+        writer.writerow({'msg': commit.msg, 'files': commit.files, 'lines': commit.lines, 'committer_date': commit.committer_date, 'modified_files': modified_file, 'merge': commit.merge, 'branches': branches})
