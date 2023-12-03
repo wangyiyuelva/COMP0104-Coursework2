678be8c69119ed3290fed894fbd49c4f4fd4a842
Cenxn <1017435995@qq.com>
2023-12-03 19:05:56 +0000

    create analyze csv

--- analyse_csv.py
+++ analyse_csv.py
@@ -4,7 +4,7 @@ import ast
 from datetime import datetime
 
 # Increase the maximum field size limit
-csv.field_size_limit(sys.maxsize)
+# csv.field_size_limit(sys.maxsize)
 
 # Function to determine if a filename represents a test file
 def is_test_file(filename):
@@ -22,6 +22,8 @@ def get_impl_filename(test_filename):
 def analyze_test_file_creation(csv_path):
     # Dictionary to store the creation times of files
     file_creation_times = {}
+    # Dictionary to store the hash with corresponding creation time
+    file_time_hash = {}
 
     # Read the CSV file
     with open(csv_path, mode='r', newline='') as file:
@@ -35,29 +37,44 @@ def analyze_test_file_creation(csv_path):
             # Record the creation date for all files
             for filename in modified_files:
                 # Assume that the file is new if not present in the dictionary
-                if filename not in file_creation_times:
-                    file_creation_times[filename] = commit_date
+                if filename[0] not in file_creation_times:
+                    file_creation_times[filename[0]] = commit_date
+                    file_time_hash[commit_date] = row['hash']
 
-    # Initialize counters for before, after, and same commit
-    before_count = after_count = same_commit_count = 0
+    with open('./pydrillerData/commons-geometry_analyze.csv', 'w', newline='') as file:
+        fields = ['type', 'hash_test_creation', 'hash_test_impl']
+        writer = csv.DictWriter(file, fieldnames=fields)
+        writer.writerow({'type': 'type', 'hash_test_creation': 'hash_test_creation', 'hash_test_impl': 'hash_test_impl'})
 
-    # Analyze the creation times by comparing test files with their corresponding implementation files
-    for filename, commit_date in file_creation_times.items():
-        if is_test_file(filename):
-            impl_filename = get_impl_filename(filename)
-            if impl_filename and impl_filename in file_creation_times:
-                impl_commit_date = file_creation_times[impl_filename]
-                if commit_date < impl_commit_date:
-                    before_count += 1
-                elif commit_date > impl_commit_date:
-                    after_count += 1
-                else:
-                    same_commit_count += 1
+        # Initialize counters for before, after, and same commit
+        before_count = after_count = same_commit_count = 0
 
-    return before_count, after_count, same_commit_count
+        # Analyze the creation times by comparing test files with their corresponding implementation files
+        for filename, commit_date in file_creation_times.items():
+            if is_test_file(filename):
+                impl_filename = get_impl_filename(filename)
+                if impl_filename and impl_filename in file_creation_times:
+                    impl_commit_date = file_creation_times[impl_filename]
+                    if commit_date < impl_commit_date:
+                        before_count += 1
+                        writer.writerow({'type': 'before',
+                                         'hash_test_creation': file_time_hash[commit_date],
+                                         'hash_test_impl': file_time_hash[impl_commit_date]})
+                    elif commit_date > impl_commit_date:
+                        after_count += 1
+                        writer.writerow({'type': 'after',
+                                         'hash_test_creation': file_time_hash[commit_date],
+                                         'hash_test_impl': file_time_hash[impl_commit_date]})
+                    else:
+                        same_commit_count += 1
+                        writer.writerow({'type': 'same',
+                                         'hash_test_creation': file_time_hash[commit_date],
+                                         'hash_test_impl': file_time_hash[impl_commit_date]})
+
+        return before_count, after_count, same_commit_count
 
 # Path to the CSV file - replace with the correct path
-csv_file_path = 'pydrillerData/my.csv'
+csv_file_path = 'pydrillerData/commons-geometry_without_change_type.csv'
 
 # Perform the analysis
 before, after, same_commit = analyze_test_file_creation(csv_file_path)
