bf85bcfe07b7e12e64209d2403316b9d18243e9a
liudiudiu <bambooliulala@gmail.com>
2023-12-05 00:31:44 +0000

    add command line arguments and scripts to ease execution

--- analyse_csv.py
+++ analyse_csv.py
@@ -2,9 +2,11 @@ import csv
 import sys
 import ast
 from datetime import datetime
+import sys
 
 # Increase the maximum field size limit
 # csv.field_size_limit(sys.maxsize)
+csv.field_size_limit(131072 * 10)
 
 # Function to determine if a filename represents a test file
 def is_test_file(filename):
@@ -26,7 +28,7 @@ def analyze_test_file_creation(csv_path):
     file_time_hash = {}
 
     # Read the CSV file
-    with open(csv_path, mode='r', newline='') as file:
+    with open('./pydrillerData/' + csv_path, mode='r', newline='', encoding='utf-8') as file:
         reader = csv.DictReader(file)
         for row in reader:
             # Parse the committer date
@@ -41,7 +43,8 @@ def analyze_test_file_creation(csv_path):
                     file_creation_times[filename[0]] = commit_date
                     file_time_hash[commit_date] = row['hash']
 
-    with open('./pydrillerData/commons-geometry_analyze.csv', 'w', newline='') as file:
+    new_file_path = './pydrillerData/analyse_' + csv_path
+    with open(new_file_path, 'w', newline='') as file:
         fields = ['type', 'hash_test_creation', 'test_file', 'hash_test_impl', 'impl_file']
         writer = csv.DictWriter(file, fieldnames=fields)
         writer.writerow({'type': 'type', 'hash_test_creation': 'hash_test_creation', 'test_file': 'test_file',
@@ -80,11 +83,17 @@ def analyze_test_file_creation(csv_path):
 
         return before_count, after_count, same_commit_count
 
+project = sys.argv[1]
 # Path to the CSV file - replace with the correct path
-csv_file_path = 'pydrillerData/commons-geometry_without_change_type.csv'
+csv_file_path = project + '.csv'
 
 # Perform the analysis
 before, after, same_commit = analyze_test_file_creation(csv_file_path)
+with open('results.txt', 'a') as file:
+    file.write(project + '\n')
+    file.write(f"Test files created before implementation files: {before}\n")
+    file.write(f"Test files created after implementation files: {after}\n")
+    file.write(f"Test files created in the same commit as implementation files: {same_commit}\n")
 
 # Output the results
 print(f"Test files created before implementation files: {before}")
--- collectInformation.py
+++ collectInformation.py
@@ -1,14 +1,18 @@
 from pydriller import Repository
 import csv
 from tqdm import tqdm
+import sys
 
-with open('./pydrillerData/commons-geometry_without_change_type.csv', 'w', newline='') as file:
+project = sys.argv[1]
+print(project)
+csv_path = './pydrillerData/' + project + '.csv'
+with open(csv_path, 'w', newline='', encoding='utf-8') as file:
     fields = ['hash', 'msg', 'files', 'lines', 'committer_date', 'modified_files', 'merge', 'branches']
     writer = csv.DictWriter(file, fieldnames=fields)
     writer.writerow(
         {'hash': 'hash', 'msg': 'msg', 'files': 'files', 'lines': 'lines', 'committer_date': 'committer_date',
          'modified_files': 'modified_file', 'merge': 'merge', 'branches': 'branches'})
-    repo_url = 'https://github.com/apache/commons-geometry'
+    repo_url = 'https://github.com/apache/' + project
     for commit in tqdm(Repository(repo_url).traverse_commits()):
         modified_file = []
         branches = []
